4e1304bc868b6832fbccea6a9e5a89b3d603173c
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-04-05 15:05:34 -0700

    Added Node class

--- ai.cpp
+++ ai.cpp
@@ -1,8 +1,28 @@
 #include <string>
 #include <iomanip>
 #include <iostream>
+#include <vector>
 using namespace std;
 
+class Node
+{
+public:
+    int value;
+    vector<Node*> children;
+    int board[9];
+
+    Node(int *arr)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            this->board[i] = *(arr+i);
+        }
+    }
+    void addChild(Node *child)
+    {
+        children.push_back(child);
+    }
+};
 
 // Calculate the utility value of the current board state.
 // Implement and return the utility value of the current board state.
@@ -18,3 +38,18 @@ float minimax(char board){
     
 }
 
+/*
+ Process: Creates children for a node based on actions
+ Input:
+    actions: vector containing possible actions.
+    head: The node to be expanded.
+ Output: No output. Creates node children.
+*/
+void createChildren(vector<int> actions, Node *head) {
+
+    for(int i = 0; i < static_cast<int>(actions.size()); i++) {
+        int *arr = result(head->board, actions[i]);
+        head->addChild(new Node(arr));
+
+    }
+}
